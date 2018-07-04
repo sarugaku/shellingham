24acf49c558deb32f80b1cd2c6dca341886b03f0
Tzu-ping Chung <uranusjr@gmail.com>
2018-07-04 21:38:37 +0800

    Correct login-shell parsing from process info
    This should fix pypa/pipenv#2496.

--- src/shellingham/posix/__init__.py
+++ src/shellingham/posix/__init__.py
@@ -17,27 +17,33 @@ def _get_process_mapping():
     return impl.get_process_mapping()
 
 
+def _get_login_shell(proc_cmd):
+    """Form shell information from the SHELL environment variable if possible.
+    """
+    login_shell = os.environ.get('SHELL', '')
+    if login_shell:
+        proc_cmd = login_shell
+    else:
+        proc_cmd = proc_cmd[1:]
+    return (os.path.basename(proc_cmd).lower(), proc_cmd)
+
+
 def get_shell(pid=None, max_depth=6):
     """Get the shell that the supplied pid or os.getpid() is running in.
     """
     pid = str(pid or os.getpid())
     mapping = _get_process_mapping()
-    login_shell = os.environ.get('SHELL', '')
+
     for _ in range(max_depth):
         try:
             proc = mapping[pid]
         except KeyError:
             break
-        name = os.path.basename(proc.args[0]).lower()
-        if name in SHELL_NAMES:
-            return (name, proc.args[0])
-        elif proc.args[0].startswith('-'):
-            # This is the login shell. Use the SHELL environ if possible
-            # because it provides better information.
-            if login_shell:
-                name = login_shell.lower()
-            else:
-                name = proc.args[0][1:].lower()
-            return (os.path.basename(name), name)
+        proc_cmd = proc.args[0]
+        if proc_cmd.startswith('-'):    # Login shell! Let's use this.
+            return _get_login_shell(proc_cmd)
+        name = os.path.basename(proc_cmd).lower()
+        if name in SHELL_NAMES:     # The inner-most (non-login) shell.
+            return (name, proc_cmd)
         pid = proc.ppid     # Go up one level.
     return None
