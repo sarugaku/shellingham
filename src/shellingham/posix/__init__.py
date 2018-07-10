422dfa62c6ce6495edb5b5f5af973c1fa8793930
Tzu-ping Chung <uranusjr@gmail.com>
2018-07-10 12:46:23 +0800

    Make sure the argument list is valid
    Add an IndexError check around argv[0] peeking to work around a Heisenbug,
    in which a process read from /proc may have an empty argument list.
    
    Also refactored the loop logic out into an iterator to make the control
    flow clearer.

--- src/shellingham/posix/__init__.py
+++ src/shellingham/posix/__init__.py
@@ -19,6 +19,23 @@ def _get_process_mapping():
     raise ShellDetectionFailure('compatible proc fs or ps utility is required')
 
 
+def _iter_process_command(mapping, pid, max_depth):
+    """Iterator to traverse up the tree, yielding `argv[0]` of each process.
+    """
+    for _ in range(max_depth):
+        try:
+            proc = mapping[pid]
+        except KeyError:    # We've reach the root process. Give up.
+            break
+        try:
+            cmd = proc.args[0]
+        except IndexError:  # Process has no name? Whatever, ignore it.
+            pass
+        else:
+            yield cmd
+        pid = proc.ppid     # Go up one level.
+
+
 def _get_login_shell(proc_cmd):
     """Form shell information from the SHELL environment variable if possible.
     """
@@ -35,17 +52,10 @@ def get_shell(pid=None, max_depth=6):
     """
     pid = str(pid or os.getpid())
     mapping = _get_process_mapping()
-
-    for _ in range(max_depth):
-        try:
-            proc = mapping[pid]
-        except KeyError:
-            break
-        proc_cmd = proc.args[0]
+    for proc_cmd in _iter_process_command(mapping, pid, max_depth):
         if proc_cmd.startswith('-'):    # Login shell! Let's use this.
             return _get_login_shell(proc_cmd)
         name = os.path.basename(proc_cmd).lower()
         if name in SHELL_NAMES:     # The inner-most (non-login) shell.
             return (name, proc_cmd)
-        pid = proc.ppid     # Go up one level.
     return None
